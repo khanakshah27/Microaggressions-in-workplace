9730da4cef069d33058ceecc5542180295902dae
khanakshah27 <167800358+khanakshah27@users.noreply.github.com>
2025-06-23 18:02:20 +0530

    Create train_model.py

---
+++ train_model.py
@@ -0,0 +1,7 @@
+
+from mgagg import train_and_save_pipeline
+
+if __name__ == "__main__":
+    print("Starting model training...")
+    train_and_save_pipeline()
+    print("Model training completed and saved as 'microagg_model.pkl'")
