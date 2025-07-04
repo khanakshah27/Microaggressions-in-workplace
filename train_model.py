ac2ab35ed8c4ce868b14f7daf37b25f11f833142
khanakshah27 <167800358+khanakshah27@users.noreply.github.com>
2025-07-04 13:55:17 +0530

    Delete train_model.py

--- train_model.py
+++
@@ -1,7 +0,0 @@
-
-from mgagg import train_and_save_pipeline
-
-if __name__ == "__main__":
-    print("Starting model training...")
-    train_and_save_pipeline()
-    print("Model training completed and saved as 'microagg_model.pkl'")
